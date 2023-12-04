1c09bd338fe912c07eaf6e4ae5593233ce2477fe
unknown <psprock2000@gmail.com>
2023-12-04 12:41:26 +0530

    project base

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const userRouter = require('./routes/user/userRouter');
+
+const app = express();
+
+//middlewares
+
+//routes
+app.get('/', async (req, res) =>{
+    try {
+        res.send('Hey! This is the node environment that we are using.')
+    } catch (error) {
+        console.log(error)
+    }
+});
+
+//user
+
+app.use('/api/v1/users', userRouter);
+
+//accouts
+
+//transaction
+
+//error handling
+
+//listen to the server
+
+const PORT = process.env.PORT || 7000;
+app.listen(PORT, ()=>{
+    console.log(`Server is running at ${PORT}`);
+})
